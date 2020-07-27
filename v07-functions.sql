514ab7e8e9b7cd1d1934a0166a8867bb8375527f
Lucas Napoli <lucas.napoli@gmail.com>
2020-07-27 00:04:37 -0400

    SP Ready / Triggers (WIP)

--- v07-functions.sql
+++ v07-functions.sql
@@ -22,7 +22,62 @@ BEGIN
 END; 
 GO
 
+-- Testing Function
 SELECT ev.TotalEarningsbyStock('SAEX') AS Total
+
+
+/* Function to see the total Maximun Earnings*/
+IF OBJECT_ID ('ev.MaximunEarnings') IS NOT NULL  
+    DROP FUNCTION ev.MaximunEarnings;  
+GO 
+CREATE FUNCTION ev.MaximunEarnings ()  
+RETURNS int   
+AS   
+-- Returns the maximun Earnings 
+BEGIN  
+    DECLARE @ret int;  
+    SELECT   @ret = MAX (PriceSell-PriceBuy)
+    FROM ev.Performance AS pe
+    INNER JOIN dd.StockTransactions AS st
+    ON pe.ToStockTransactionID = st.ID
+    INNER JOIN dd.Stocks AS Sto
+    ON st.ToStockID = Sto.ID 
+    IF (@ret IS NULL)   
+        SET @ret = 0;  
+    RETURN @ret;  
+END; 
+GO
+
+-- Testing Function
+SELECT ev.MaximunEarnings() AS Maximun;
+GO
+
+
+/* Function to see the total Minimium Earnings*/
+IF OBJECT_ID ('ev.MinimunEarnings') IS NOT NULL  
+    DROP FUNCTION ev.MinimunEarnings;  
+GO 
+CREATE FUNCTION ev.MinimunEarnings ()  
+RETURNS int   
+AS   
+-- Returns the maximun Earnings 
+BEGIN  
+    DECLARE @ret int;  
+    SELECT   @ret = MIN (PriceSell-PriceBuy)
+    FROM ev.Performance AS pe
+    INNER JOIN dd.StockTransactions AS st
+    ON pe.ToStockTransactionID = st.ID
+    INNER JOIN dd.Stocks AS Sto
+    ON st.ToStockID = Sto.ID 
+    IF (@ret IS NULL)   
+        SET @ret = 0;  
+    RETURN @ret;  
+END; 
+GO
+
+-- Testing Function
+SELECT ev.MinimunEarnings() AS Minimuns;
+GO
 
 
 
---
+++ v08-stored-procedure.sql
@@ -0,0 +1,42 @@
+/* Total Earning by Year */
+IF OBJECT_ID ('ev.TotalEarningsbyYear') IS NOT NULL  
+    DROP PROCEDURE ev.TotalEarningsbyYear;  
+GO 
+CREATE PROCEDURE ev.TotalEarningsbyYear
+AS
+    SELECT SUM (PriceSell-PriceBuy) Earnings
+    FROM ev.Performance AS pe
+    INNER JOIN dd.StockTransactions AS st
+    ON pe.ToStockTransactionID = st.ID
+    INNER JOIN dd.Stocks AS Sto
+    ON st.ToStockID = Sto.ID
+    WHERE pe.EvaluationAt LIKE '%2020%'
+    GROUP BY pe.EvaluationAt;
+;
+GO
+
+-- Testing Procedure
+EXECUTE ev.TotalEarningsbyYear;
+GO
+
+-- DROP PROCEDURE ev.TotalEarningsbyYear;
+
+/* % of transacions with lost */
+IF OBJECT_ID ('ev.PlostTransactions') IS NOT NULL  
+    DROP PROCEDURE ev.PlostTransactions;  
+GO 
+CREATE PROCEDURE ev.PlostTransactions
+AS
+    SELECT SUM(CASE WHEN (PriceSell-PriceBuy)<0 THEN 1 ELSE 0 END)*100 / SUM(CASE WHEN (PriceSell-PriceBuy)!=0 THEN 1 ELSE 0 END) EarningsPercentageLost
+    FROM ev.Performance AS pe
+    INNER JOIN dd.StockTransactions AS st
+    ON pe.ToStockTransactionID = st.ID
+    INNER JOIN dd.Stocks AS Sto
+    ON st.ToStockID = Sto.ID
+    GROUP BY pe.EvaluationAt;
+;
+GO
+
+-- DROP PROCEDURE ev.PlostTransactions;
+EXECUTE ev.PlostTransactions;
+GO
